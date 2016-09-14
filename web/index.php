7984e80f2678a0bfadeb28ee693925c5a89daa20
Tahris G <goliat2107@gmail.com>
2016-09-13 22:28:13 -0400

    VIsta inicial modificada

--- web/index.php
+++ web/index.php
@@ -13,16 +13,3 @@ echo "string";
 (new yii\web\Application($config))->run();
 
 ?>
-<div class="row">
-    <div class="col-md-4 col-sm-4 col-xs-12">
-        <div class="info-box">
-            <span class="info-box-icon bg-orange index-image">
-                <a target="_blank" href="http://sagt.anz/otrs/index.pl"><img src="images/logos/logoOTRS.png" title="Centro De Atencion y Soporte" /></a>
-            </span>
-            <div class="info-box-content">
-              <a href="?r=naturaleza"><h4><b>Asistencia técnica.</b></h4></a>
-                <a href="http://sagt.anz/otrs/index.pl"><h4><b>Asistencia técnica.</b></h4></a>
-                <a href="http://sagt.anz/otrs/index.pl"><span class="info-box-text">Centro De Atencion y Soporte</span></a>
-            </div><!-- /.info-box-content -->
-        </div><!-- /.info-box -->
-    </div><!-- /.col -->
